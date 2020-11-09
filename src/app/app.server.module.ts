6c0dc79d0db767e46cace0d35953951306071968
Benjamin Legrand <benjilegnard@protonmail.com>
2020-11-09 19:11:47 +0100

    feat(env-config): manage environment configuration with transfer-state.

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { EnvConfig } from './core/model/env-config.model';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    ServerTransferStateModule,
+  ],
+  providers: [
+    {
+      provide: 'ENV_CONFIG_SSR',
+      useValue: { twitchUsername: process.env.TWITCH_USERNAME } as EnvConfig,
+    },
   ],
   bootstrap: [AppComponent],
 })
