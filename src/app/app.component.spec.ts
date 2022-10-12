902503e62b1eb12e2f574c2e296cb85b52a240bf
Benjamin Legrand <benjilegnard@protonmail.com>
2022-10-12 02:18:14 +0200

    test: upgrade to jest 28, fix tests failing because of delaunator

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+jest.mock('delaunator');
 import { AppComponent } from './app.component';
-
+import { BackgroundComponent } from './shared/components/background/background.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        BackgroundComponent,
       ],
     }).compileComponents();
   });
@@ -16,16 +18,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'stream-overlay'`, () => {
+  it(`should have as title 'benjilegnard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('stream-overlay');
+    expect(app.title).toEqual('benjilegnard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('stream-overlay app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('benjilegnard is streaming!');
   });
 });
