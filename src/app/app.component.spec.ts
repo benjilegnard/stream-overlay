e1a16cf04e5fd0c0c1bc257fb78ce79b6f4ff00e
Benjamin Legrand <benjilegnard@protonmail.com>
2022-11-15 20:37:18 +0100

    fix: remove "is streaming" from title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,6 +28,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('benjilegnard is streaming!');
+    expect(compiled.querySelector('h1').textContent).toContain('benjilegnard');
   });
 });
